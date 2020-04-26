5a824b1e3acdfe33b9e117b7c9eecb50f5fa9e8b
刘佳伊 <simplelive2013@126.com>
2020-04-26 23:48:28 +0800

    Create 01-twoSum.js

---
+++ 01-twoSum.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let res=[]
+    let hashmap=new Map()
+    nums.forEach(item=>{
+        hashmap.set(item,1)
+    })
+    for(let item in nums){
+        let temp=target-nums[item]
+        nums.splice(item,1)
+        if(hashmap.has(temp)){
+            res.push(item)
+        }
+    }
+    return res
+};
