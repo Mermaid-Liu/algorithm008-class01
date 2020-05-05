f709fba646568cf2d277c8aaf8e5a28bc8e67051
刘佳伊 <simplelive2013@126.com>
2020-05-06 06:42:18 +0800

    Create 102-实战-levelOrder.py

---
+++ Week_04/102-实战-levelOrder.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        levels=[]
+        #terminator
+        if not root:
+            return levels
+        def helper(node,level):
+            #start with current level
+            if len(levels)==level:
+                levels.append([])
+            
+            #append the current node value
+            levels[level].append(node.val)
+
+            #process child nodes for the next level
+            if node.left:
+                helper(node.left,level+1)
+            if node.right:
+                helper(node.right,level+1)
+        helper(root,0)
+        return levels
