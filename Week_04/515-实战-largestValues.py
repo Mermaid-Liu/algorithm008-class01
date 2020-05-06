45896ad9a2ab45007e8e62ab9c48cb93bfd2d5f9
刘佳伊 <simplelive2013@126.com>
2020-05-06 13:24:31 +0800

    Create 515-实战-largestValues.py

---
+++ Week_04/515-实战-largestValues.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def largestValues(self, root: TreeNode) -> List[int]:
+        if not root:
+            return []
+        result=[]
+        q=[(root,0)]
+        while q:
+            curr,level=q.pop(0)
+            if level==len(result):
+                result.append(curr.val)
+            result[level]=max(result[level],curr.val)
+            if curr.left:
+                q.append((curr.left,level+1))
+            if curr.right:
+                q.append((curr.right,level+1))
+        return result
