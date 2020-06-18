dc05d6f2810ca1c18377f42e089c49e0230c53bd
刘佳伊 <simplelive2013@126.com>
2020-06-18 17:36:52 +0800

    Update uniquePaths.py

--- Week_06/uniquePaths.py
+++ Week_06/uniquePaths.py
@@ -1,7 +1,6 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         dp=[[1 for x in range(n)]for x in range(m)]
-        print(dp)
         for i in range(1,m):
             for j in range(1,n):
                 dp[i][j]=dp[i][j-1]+dp[i-1][j]
