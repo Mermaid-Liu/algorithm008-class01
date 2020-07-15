00039a774759c0f16ad761f01950158bccd814b8
刘佳伊 <simplelive2013@126.com>
2020-07-15 22:07:03 +0800

    Update 69-mySqrt.py

--- Week_04/69-mySqrt.py
+++ Week_04/69-mySqrt.py
@@ -1,12 +1,11 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        if x==0:
-            return 0
+        if x<2:
+            return x
         left,right=1,x//2
         while left<right:
             mid=(left+right+1)>>1
-            square=mid*mid
-            if square>x:
+            if mid*mid>x:
                 right=mid-1
             else:
                 left=mid
